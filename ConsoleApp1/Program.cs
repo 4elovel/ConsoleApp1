2a1286eb03517ad8d6052d026a88d5beea4f6cd0
4elovel <marcynakvova007@gmail.com>
2023-03-27 18:34:44 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,27 @@
+﻿
+using System;
+using static System.Console;
+class MyClass
+{
+    static void Main()
+    {
+        Int16[] mas = { 1, -1, 0, 2, 3, -4, 5, 4, -8, 7, 11 };
+        WriteLine("Plus: ");
+        foreach (Int16 i in mas)
+        {
+            if (i > 0)
+            {
+                Write($"{i} ");
+            }
+        }
+        WriteLine();
+        WriteLine("Minus: ");
+        foreach (Int16 i in mas)
+        {
+            if (i < 0)
+            {
+                Write($"{i} ");
+            }
+        }
+    }
+}
